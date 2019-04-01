703634e1af45610dd7cf93aca14867f7dbdf4737
Eri Jonhson <eri.silva@ccc.ufcg.edu.br>
2019-04-01 17:14:14 -0300

    Without mail services to help in private networks context

--- src/main/java/br/edu/opi/manager/comunication/email/SenderMailAsync.java
+++ src/main/java/br/edu/opi/manager/comunication/email/SenderMailAsync.java
@@ -1,40 +1,40 @@
 package br.edu.opi.manager.comunication.email;
 
-import br.edu.opi.manager.utils.BeanUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+// import br.edu.opi.manager.utils.BeanUtil;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.mail.javamail.JavaMailSender;
 
 public class SenderMailAsync extends Thread {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SenderMailAsync.class.getSimpleName());
+	// private static final Logger LOGGER = LoggerFactory.getLogger(SenderMailAsync.class.getSimpleName());
 
-	@Autowired
-	private JavaMailSender mailSender;
+	// @Autowired
+	// private JavaMailSender mailSender;
 
-	private SimpleMailMessage message;
+	// private SimpleMailMessage message;
 
-	public SenderMailAsync(SimpleMailMessage message) {
-		BeanUtil.autowire(this, this.mailSender);
-		this.message = message;
-	}
+	// public SenderMailAsync(SimpleMailMessage message) {
+	// 	BeanUtil.autowire(this, this.mailSender);
+	// 	this.message = message;
+	// }
 
-	public void run() {
-		try {
-			mailSender.send(message);
-		} catch (Exception e) {
-			LOGGER.error(e.getStackTrace().toString());
-		}
-	}
+	// public void run() {
+	// 	try {
+	// 		mailSender.send(message);
+	// 	} catch (Exception e) {
+	// 		LOGGER.error(e.getStackTrace().toString());
+	// 	}
+	// }
 
-	public SimpleMailMessage getMessage() {
-		return message;
-	}
+	// public SimpleMailMessage getMessage() {
+	// 	return message;
+	// }
 
-	public void setMessage(SimpleMailMessage message) {
-		this.message = message;
-	}
+	// public void setMessage(SimpleMailMessage message) {
+	// 	this.message = message;
+	// }
 
 }
