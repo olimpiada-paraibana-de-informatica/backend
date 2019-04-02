6604490a69421dfc6e38221b76ca9c80f8c88482
Eri Jonhson <eri.silva@ccc.ufcg.edu.br>
2019-04-02 13:07:01 -0300

    Adding user verification before create

--- src/main/java/br/edu/opi/manager/school/exception/ErrorConstants.java
+++ src/main/java/br/edu/opi/manager/school/exception/ErrorConstants.java
@@ -6,5 +6,6 @@ public class ErrorConstants {
 	public static final String DELEGATE_NOT_NULL = "DELEGATE_NOT_NULL";
 	public static final String DELEGATE_NOT_FOUND = "DELEGATE_NOT_FOUND";
 	public static final String DELEGATE_ALREADY_EXISTS = "DELEGATE_ALREADY_EXISTS";
+	public static final String USER_NOT_DELEGATE = "USER_NOT_DELEGATE";
 
 }
--- src/main/java/br/edu/opi/manager/school/service/SchoolService.java
+++ src/main/java/br/edu/opi/manager/school/service/SchoolService.java
@@ -6,8 +6,11 @@ import br.edu.opi.manager.delegate.service.DelegateService;
 import br.edu.opi.manager.project_patterns.services.GenericService;
 import br.edu.opi.manager.school.exception.DelegateNotNullRuntimeException;
 import br.edu.opi.manager.school.exception.SchoolExistsRuntimeException;
+import br.edu.opi.manager.school.exception.UserNotDelegateRuntimeException;
 import br.edu.opi.manager.school.model.School;
 import br.edu.opi.manager.school.repository.SchoolRepository;
+import br.edu.opi.manager.user.model.UserModel;
+import br.edu.opi.manager.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +21,22 @@ public class SchoolService extends GenericService<Long, School, SchoolRepository
 
 	private DelegateRepository delegateRepository;
 
+	private UserRepository userRepository;
+
 	@Autowired
 	public SchoolService(
 			DelegateRepository delegateRepository,
-			DelegateService delegateService) {
+			DelegateService delegateService,
+			UserRepository userRepository) {
 		this.delegateRepository = delegateRepository;
 		this.delegateService = delegateService;
+		this.userRepository = userRepository;
 	}
 
 	@Override
 	public void validateBeforeCreate(School school) {
 		solveSchool(school);
+		solveCommonUser(school);
 		solveDelegate(school);
 	}
 
@@ -41,8 +49,22 @@ public class SchoolService extends GenericService<Long, School, SchoolRepository
 	public void validateBeforeDelete(Long aLong) {
 	}
 
+	private void solveCommonUser(School school) {
+		Delegate delegate = school.getDelegate();
+		if (delegate == null || delegate.getUsername() == null) {
+			throw new DelegateNotNullRuntimeException();
+		}
+		UserModel user = userRepository.findByUsername(delegate.getUsername());
+		if (user != null) {
+			throw new UserNotDelegateRuntimeException();
+		}
+	}
+
 	private void solveSchool(School school) {
 		Delegate delegate = school.getDelegate();
+		if (delegate == null || delegate.getUsername() == null) {
+			throw new DelegateNotNullRuntimeException();
+		}
 		School savedSchool = repository.findByDelegateUsername(delegate.getUsername());
 		if (savedSchool != null) {
 			throw new SchoolExistsRuntimeException(savedSchool.getName());
