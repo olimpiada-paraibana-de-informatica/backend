af2bbf9d5abe2f4b1bc6edc443dd6dde6b08c503
Eri Jonhson <erijonhson.os@gmail.com>
2019-05-05 22:35:45 -0300

    Adjusting mail sender ok

--- src/main/java/br/edu/opi/manager/comunication/email/SenderMailService.java
+++ src/main/java/br/edu/opi/manager/comunication/email/SenderMailService.java
@@ -1,34 +1,65 @@
 package br.edu.opi.manager.comunication.email;
 
-// import br.edu.opi.manager.delegate.models.Delegate;
-// import org.springframework.mail.SimpleMailMessage;
-// import org.springframework.stereotype.Component;
+import br.edu.opi.manager.delegate.models.Delegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
 
-//@Component
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Component
 public class SenderMailService {
 
-	// public void send(String userEmail, String subject, String text) {
-	// 	SimpleMailMessage message = new SimpleMailMessage();
-	// 	message.setTo(userEmail);
-	// 	message.setSubject(subject);
-	// 	message.setText(text);
-	// 	SenderMailAsync sender = new SenderMailAsync(message);
-	// 	sender.start();
-	// }
+	private static final Logger LOGGER = LoggerFactory.getLogger(SenderMailService.class.getSimpleName());
 
-	// public void sendToList(Delegate delegate) {
-	// 	// TODO: verify with Rohit and/or Raquel
-	// 	SimpleMailMessage message = new SimpleMailMessage();
-	// 	message.setTo("eri.silva@ccc.ufcg.edu.br");
-	// 	message.setSubject("[OPI] Nova conta criada");
-	// 	String text = "\t" + delegate.getFullName() +
-	// 			" registrou um conta para o e-mail " +
-	// 			delegate.getUsername() + ".\n\n" +
-	// 			"\tMais detalhes em: http://opi.org/t=asd23jnbisjk3j4kjnksa\n\n" +
-	// 			"Att Equipe OPI";
-	// 	message.setText(text);
-	// 	SenderMailAsync sender = new SenderMailAsync(message);
-	// 	sender.start();
-	// }
+	private ExecutorService workers;
+	private JavaMailSender mailSender;
+
+	@Autowired
+	public SenderMailService(
+			@Value("${mail.workers.maxPoolSize}") final Integer maxPoolSize,
+			JavaMailSender mailSender) {
+		this.workers = Executors.newFixedThreadPool(maxPoolSize);
+		this.mailSender = mailSender;
+	}
+
+	public void send(String userEmail, String subject, String text) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(userEmail);
+		message.setSubject(subject);
+		message.setText(text);
+		senderEmailAsync(message);
+	}
+
+	public void sendToList(Delegate delegate) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		// "rohitgheyi@gmail.com", "raquel.paz@ccc.ufcg.edu.br"
+		message.setTo("eri.silva@ccc.ufcg.edu.br"); // TODO: solve with CRUD
+		message.setSubject("[OPI] Nova conta criada");
+		String text = "Olá.\n\n\t" + delegate.getName() +
+				" registrou um conta para o e-mail " +
+				delegate.getUsername() + ".\n\n" +
+				"\tMais detalhes em: https://opiintern.herokuapp.com\n\n" +
+				"Att,\n"+
+				"Equipe OPI";
+		message.setText(text);
+		senderEmailAsync(message);
+	}
+
+	private void senderEmailAsync(SimpleMailMessage message) {
+		workers.execute(() -> {
+			try {
+				mailSender.send(message);
+			} catch (Exception e) {
+				LOGGER.error(e.getStackTrace().toString());
+				// TODO: email rec strategy
+			}
+		});
+	}
 
 }
--- src/main/java/br/edu/opi/manager/delegate/models/DelegateListener.java
+++ src/main/java/br/edu/opi/manager/delegate/models/DelegateListener.java
@@ -1,32 +1,32 @@
 package br.edu.opi.manager.delegate.models;
 
-// import br.edu.opi.manager.comunication.email.SenderMailService;
-// import br.edu.opi.manager.utils.BeanUtil;
-// import org.springframework.beans.factory.annotation.Autowired;
+import br.edu.opi.manager.comunication.email.SenderMailService;
+import br.edu.opi.manager.utils.BeanUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.PostPersist;
 
 public class DelegateListener {
 
-	// @Autowired
-	// SenderMailService senderMailService;
+	@Autowired
+	SenderMailService senderMailService;
 
-	// private final String subject = "[OPI] Bem vindo à Olimpíada Paraíbana de Informática";
+	private final String subject = "[OPI] Bem vindo à Olimpíada Paraíbana de Informática";
 
 	@PostPersist
 	public void methodAfterCreate(Delegate delegate) {
-		// BeanUtil.autowire(this, this.senderMailService);
-		// String email = delegate.getUsername();
-		// // TODO: refactoring e-mail content
-		// String text = "\tOlá " + delegate.getFullName() + "," +
-		// 		" uma conta foi criada em seu nome.\n" +
-		// 		"\tPara confirmar seus dados basta clicar no link abaixo:\n" +
-		// 		"\t\thttp://opi.org/asjlkjnlkj3nrlwjw378\n\n" +
-		// 		"Se não foi você, favor desconsiderar esse e-mail.\n\n" +
-		// 		"Até breve,\n" +
-		// 		"Equipe OPI";
-		// senderMailService.send(email, subject, text);
-		// senderMailService.sendToList(delegate);
+		BeanUtil.autowire(this, this.senderMailService);
+		String email = delegate.getUsername();
+		// TODO: refactoring e-mail content with CRUD
+		String text = "Olá " + delegate.getName() + "," +
+				"\n\n\tUma conta foi criada em seu nome.\n" +
+				"\tPara confirmar seus dados basta clicar no link abaixo:\n" +
+				"\t\thttps://opiintern.herokuapp.com/u=asjlkjnlkj3nrlwjw378\n\n" + // TODO: solve link
+				"\tSe não foi você quem criou a conta, favor desconsiderar esse e-mail.\n\n" +
+				"Att,\n" +
+				"Equipe OPI";
+		senderMailService.send(email, subject, text);
+		senderMailService.sendToList(delegate);
 	}
 
 }
