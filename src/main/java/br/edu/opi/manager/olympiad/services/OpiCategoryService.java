705f694ec83ec05768255e4485c8146a35204f48
Eri Jonhson <eri.silva@ccc.ufcg.edu.br>
2019-05-27 17:34:49 -0300

    Refixing

--- src/main/java/br/edu/opi/manager/olympiad/services/OpiCategoryService.java
+++ src/main/java/br/edu/opi/manager/olympiad/services/OpiCategoryService.java
@@ -3,16 +3,19 @@ package br.edu.opi.manager.olympiad.services;
 import br.edu.opi.manager.olympiad.models.OpiCategory;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class OpiCategoryService {
 
 	public List<OpiCategory> index() {
-		List<OpiCategory> categories = Arrays.asList(OpiCategory.values());
-		categories.remove(OpiCategory.INICIACAO_1_PUB);
-		categories.remove(OpiCategory.INICIACAO_2_PUB);
+		List<OpiCategory> categories = new LinkedList<>();
+		for (OpiCategory category : OpiCategory.values()) {
+			if (!category.equals(OpiCategory.INICIACAO_1_PUB) && !category.equals(OpiCategory.INICIACAO_2_PUB)) {
+				categories.add(category);
+			}
+		}
 		return categories;
 	}
 
